e052bcd4924d135f0616c60e81e9cc38b65634f9
AkiraBrown <akira.watson.brown@gmail.com>
2024-02-22 15:21:43 -0500

    AB-1521-Created a local storage system for scrapped results

--- queries/serp.js
+++ queries/serp.js
@@ -1,9 +1,28 @@
-const db = require("../db/dbConfig");
+// const db = require("../db/dbConfig");
+const fs = require("fs");
+const Axios = require("axios");
 require("dotenv").config();
+//const baseUrl = "https://serpapi.com/search.json?engine=google_shopping";
 const mainUserSearch = async ({ search }) => {
-  try {
-  } catch (error) {
-    return error;
+  if (fs.existsSync(`./search_data/${search.toLowerCase()}.json`)) {
+    return JSON.parse(
+      fs.readFileSync(`./search_data/${search.toLowerCase()}.json`)
+    );
+  } else {
+    try {
+      const result = await Axios.get(
+        `https://serpapi.com/search.json?engine=google_shopping&api_key=${
+          process.env.SERP_API_KEY
+        }&q=${search.toLowerCase()}`
+      );
+      fs.writeFileSync(
+        `./search_data/${search}.json`,
+        JSON.stringify(result.data.shopping_results)
+      );
+      return result.data.shopping_results;
+    } catch (error) {
+      return error;
+    }
   }
 };
 
